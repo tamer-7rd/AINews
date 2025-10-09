56b0a77908f6e11995d6faabe81d3abb261bceac
Tamerlan <tamerlan4496@gmail.com>
2025-10-09 19:15:19 +0400

    chore: save before codemod

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    printWidth: 100,
+    quoteProps: 'as-needed', 
+    bracketSpacing: true,  
+    bracketSameLine: false,     
+  }
